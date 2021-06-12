549e22077dfaad11c6c3d0e18f8654c003c13c09
sang-w0o <robbyra@gmail.com>
2021-06-12 18:28:18 +0900

    Create Saga functions

--- src/containers/saga.ts
+++ src/containers/saga.ts
@@ -1,7 +1,21 @@
-import { call, put } from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 import { getWeather } from "../global/api";
+import { WeatherResponse } from "../global/interface";
 
 function* getWeatherSaga() {
-  yield put;
-  yield call(getWeather);
+  yield put({ type: "weather/setLoading", payload: true });
+  try {
+    const response: WeatherResponse = yield call(getWeather);
+    yield put({ type: "weather/setData", payload: response });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    yield put({ type: "weather/setLoading", payload: false });
+  }
 }
+
+function* weatherSaga(): Generator {
+  yield takeLatest("weather/getWeatherSaga", getWeatherSaga);
+}
+
+export default weatherSaga;
